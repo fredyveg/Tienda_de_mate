5247c454589c25c8f06a0a6fdaa8b7753fa535a7
Jose lago <alfredol.vega@yahoo.com.ar>
2024-11-04 18:18:29 -0300

    Update constants.tsx
    se cambia img de archivo constants

--- app/resources/constants.tsx
+++ app/resources/constants.tsx
@@ -13,61 +13,61 @@ export const PRODUCTOS = [
     id: 1,
     name: "Yerba Mate Clásica",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/1e733d3a2db28f647fe223010363cfda/pol_il_8104-8104.webp",
     description:
       "¡EL SABOR DE LA TRADICIÓN EN CADA SORBO! SI BUSCAS UNA EXPERIENCIA AUTÉNTICA, LA YERBA MATE CLÁSICA ES PARA TI. PERFECTA PARA CUALQUIER MOMENTO DEL DÍA, CON EL EQUILIBRIO IDEAL DE AMARGOR Y FRESCURA.",
-    color: "bg-[#6B4226]",
+    color: "bg-[#eafad7]",
   },
   {
     id: 2,
     name: "Yerba Mate Con Hierbas",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/ee62cdbd95e7f4d4bee6ea1da050b538/pol_il_7275-7275.webp",
     description:
       "UN TOQUE HERBAL PARA REVITALIZARTE. ESTA COMBINACIÓN DE YERBA MATE CON HIERBAS SELECCIONADAS TE DARÁ UN IMPULSO EXTRA, IDEAL PARA COMENZAR EL DÍA CON ENERGÍA RENOVADA Y UNA FRESCURA NATURAL.",
-    color: "bg-[#4CAF50]",
+    color: "bg-[#c0f584]",
   },
   {
     id: 3,
     name: "Yerba Mate Orgánica",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/db2a234433c9da81cc46cba87c0eac20/pol_il_5713-5713.webp",
     description:
       "NATURAL Y PURA, TAL COMO LA NATURALEZA LA OFRECE. CULTIVADA SIN PESTICIDAS NI ADITIVOS, ESTA YERBA MATE ES IDEAL PARA QUIENES BUSCAN UN ESTILO DE VIDA SALUDABLE, RESPETANDO EL MEDIO AMBIENTE EN CADA MATE.",
-    color: "bg-[#2E7D32]",
+    color: "bg-[#446125]",
   },
   {
     id: 4,
     name: "Yerba Mate Suave",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/cc99210686003b8e7375d3a93bc68a00/pol_il_5642-5642.webp",
     description:
       "EL EQUILIBRIO PERFECTO ENTRE SUAVIDAD Y SABOR. DISFRUTA DE UN MATE MÁS SUAVE AL PALADAR SIN PERDER EL GUSTO TRADICIONAL. PERFECTA PARA QUIENES PREFIEREN UN MATE MÁS LIGERO PERO LLENO DE SABOR.",
-    color: "bg-[#FFC107]",
+    color: "bg-[#73de00]",
   },
   {
     id: 5,
     name: "Yerba Mate Energética",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/9c919d960df17770e7ac56aabc3de168/pol_il_8103-8103.webp",
     description:
       "¡PODER EN CADA SORBO! IDEAL PARA LOS QUE BUSCAN UN IMPULSO EXTRA EN SU DÍA, ESTA YERBA ESTÁ DISEÑADA PARA DARTE ENERGÍA Y CONCENTRACIÓN, SIN DEJAR DE LADO EL SABOR TRADICIONAL.",
-    color: "bg-[#FF5722]",
+    color: "bg-[#3cff00]",
   },
   {
     id: 6,
     name: "Yerba Mate con Menta",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/e201fb1cc3b694532f016f7273b9d7d1/pol_il_8165-8165.webp",
     description:
       "REFRESCANTE Y VIGORIZANTE. LA FRESCURA DE LA MENTA SE COMBINA A LA PERFECCIÓN CON LA YERBA MATE, CREANDO UNA MEZCLA PERFECTA PARA DISFRUTAR EN DÍAS CALUROSOS O PARA REFRESCAR TU MENTE.",
-    color: "bg-[#00BCD4]",
+    color: "bg-[#00ffa2]",
   },
   {
     id: 7,
     name: "Yerba Mate con Limón",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/1a5ddc765d078d5580d89963017134f1/pol_il_7266-7266.webp",
     description:
       "ÁCIDO, REFRESCANTE Y ÚNICO. EL TOQUE CÍTRICO DEL LIMÓN APORTA UNA NUEVA DIMENSIÓN DE FRESCURA A TU MATE, HACIÉNDOLO LA ELECCIÓN PERFECTA PARA QUIENES DISFRUTAN DE SABORES VIBRANTES Y REFRESCANTES.",
     color: "bg-[#FFEB3B]",
@@ -76,27 +76,18 @@ export const PRODUCTOS = [
     id: 8,
     name: "Yerba Mate Despalada",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/902bad74110fac0c545255241a931383/pol_il_7263-7263.webp",
     description:
       "PURA, INTENSA Y SIN PALOS. ESTA YERBA MATE ESTÁ COMPUESTA EXCLUSIVAMENTE POR HOJAS, LO QUE LE OTORGA UN SABOR MÁS FUERTE E INTENSO. IDEAL PARA LOS AMANTES DE UN MATE PURO Y PODEROSO.",
-    color: "bg-[#795548]",
+    color: "bg-[#daf29d]",
   },
   {
     id: 9,
     name: "Yerba Mate con Jengibre",
     image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
+      "https://www.matemundo.es/hpeciai/55d27faac226cc8c204b646992cc0e06/pol_il_7262-7262.webp",
     description:
       "PICANTE Y ENERGIZANTE. UNA COMBINACIÓN ÚNICA DE YERBA MATE CON EL TOQUE PICANTE DEL JENGIBRE, PERFECTA PARA QUIENES BUSCAN UNA BEBIDA REVITALIZANTE Y LLENA DE SABOR.",
-    color: "bg-[#FF9800]",
-  },
-  {
-    id: 10,
-    name: "Yerba Mate Ahumada",
-    image:
-      "https://th.bing.com/th/id/OIG4.6lddpQ20ThHlooDWB5z1?w=1024&h=1024&rs=1&pid=ImgDetMain",
-    description:
-      "UNA EXPERIENCIA DE SABOR INTENSA. CON UN SABOR ROBUSTO Y AHUMADO, ESTA YERBA MATE ES IDEAL PARA AQUELLOS QUE BUSCAN UNA EXPERIENCIA MÁS PROFUNDA Y COMPLEJA EN CADA MATE.",
-    color: "bg-[#37474F]",
+    color: "bg-[#b7ff00]",
   },
 ];
