f2343f87e0cc04095b0575e05ef1546c58b8407d
Jose lago <alfredol.vega@yahoo.com.ar>
2024-11-04 21:18:50 -0300

    Update ProductSlider.tsx
    se cambia pading

--- app/components/ProductSlider.tsx
+++ app/components/ProductSlider.tsx
@@ -46,7 +46,7 @@ export default function ProductSlider() {
       <div className="flex flex-col xl:flex-row w-full py-10 gap-8">
         <Card className="w-full h-96 rounded-none shadow-none bg-cover bg-[url('https://yerbamateargentina.org.ar/imagenes/archivos/noticias/79192_imagen_1227x747xrecortarxagrandar.jpg?random=1615376832')]">
           <CardBody className="flex flex-col justify-end gap-4 items-start p-8">
-            <p className="bg-blanco px-2 py-1 text-primario-300 text-lg">
+            <p className="bg-blanco px-2 text-primario-300 text-lg">
               Aprende a hacer tu mate como un experto
             </p>
             <h1 className="text-4xl font-semibold text-blanco">
@@ -62,7 +62,7 @@ export default function ProductSlider() {
         </Card>
         <Card className="w-full h-96 rounded-none shadow-none bg-cover bg-[url('https://media.ambito.com/p/5891322c9b9c38abd24864874a7e6948/adjuntos/239/imagenes/039/792/0039792172/1200x675/smart/mate1jpg.jpg')]">
           <CardBody className="flex flex-col justify-end gap-4 items-start p-8">
-            <p className="bg-blanco px-2 py-1 text-primario-300 text-lg">
+            <p className="bg-blanco px-2 text-primario-300 text-lg">
               Conoce todo lo que podes encontrar en nuestros locales
             </p>
             <h1 className="text-4xl font-semibold text-blanco">
