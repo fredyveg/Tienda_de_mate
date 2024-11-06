b5987dce24bde4cfcbf8232d85dde49200f09bc1
Jose lago <alfredol.vega@yahoo.com.ar>
2024-11-06 20:04:27 -0300

    header dentro de "nuestro menu"
    se agrega header dentro de seccion nuestro menu

--- app/components/OurMenu.tsx
+++ app/components/OurMenu.tsx
@@ -1,19 +1,37 @@
 "use client";
 // React
 import React from "react";
-import {MENU_PRODUCTOS} from "../resources/constants";
+import { MENU_PRODUCTOS } from "../resources/constants";
 
 // Componente OurMenu
 export default function OurMenu() {
   return (
     <div className="flex flex-col gap-4 bg-white">
-      {/* //falta agregar header */}
+      {/*Header con imagen*/}
+      <header
+  className="relative w-full h-[40vh] sm:h-[450px] md:h-[550px] bg-cover bg-center flex items-center justify-start px-5 sm:px-10"
+  style={{
+          backgroundImage:
+            'url(https://tiendadecafe.com.ar/wp-content/uploads/2021/05/slider_platos_02.jpg?id=902)',
+          backgroundSize: '130% 145%', 
+          backgroundPosition: '60% 60%',
+        }}
+      >
+        <div className="ml-20 text-white">
+          <h1 className="text-5xl flex flex-col items-start font-light">SOMOS MATE <span className="text-[4.0rem] font-bold ">Y MUCHO MAS</span></h1>
+          <h2 className="text-xl font-light ">Veni a disfrutar nuestros platos, perfectos para<br />
+           <span className="block">cada momento de tu día.</span>
+
+</h2>
+        </div>
+      </header>
+
       <h2 className="text-xl font-bold"></h2>
       <div className="flex flex-wrap justify-center gap-4">
         {MENU_PRODUCTOS.slice(0, 10).map((url, index) => (
           <div
             key={index}
-            className="overflow-hidden rounded-lg hover:scale-105 transition-transform duration-300 w-1/2 md:w-1/3 lg:w-1/5"
+            className="overflow-hidden rounded-lg hover:scale-105 transition-transform duration-300  w-1/2 md:w-1/3 lg:w-1/5"
           >
             <img
               src={url}
